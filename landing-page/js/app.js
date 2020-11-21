41ad7017bae5bf79c27ea9e90182ec49c4df00d1
Tolga <68684561+TolgaS92@users.noreply.github.com>
2020-11-21 10:39:56 -0700

    Add files via upload

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -1,8 +1,11 @@
-
 const navigation = document.getElementById('navbar__list');
 
 const sections = document.querySelectorAll('section');
 
+
+function scrollToSection(sectionID) {
+	window.scrollTo(0, sectionID);
+}
 
 const navBuilder = () => {
 
@@ -10,9 +13,10 @@ const navBuilder = () => {
     sections.forEach(section => {
 
         const sectionID = section.id;
-        const sectionDataNav = section.dataset.nav;
+      const sectionDataNav = section.dataset.nav;
+      let toOffSection = section.offsetTop + 30;
 
-        navUI += `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`;
+        navUI += `<li><a class="menu__link"  name="#${sectionID}" onClick="scrollToSection(${toOffSection})">${sectionDataNav}</a></li>`;
 
     });
     navigation.innerHTML = navUI;
@@ -27,14 +31,16 @@ const offset = (section) => {
 };
 
 const removeActive = (section) => {
-    section.classList.remove('your-active-class');
+    section.classList.remove('active');
     section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
 };
 
 const addActive = (conditional, section) => {
     if(conditional){
-        section.classList.add('your-active-class');
-        section.style.cssText = "background-color: grey;";
+        section.classList.add('active');
+      section.style.cssText = "background-color: grey;";
+
+      console.log("test active viewport " + section.id)
     };
 };
 
@@ -42,9 +48,7 @@ const addActive = (conditional, section) => {
 const sectionActivation = () => {
     sections.forEach(section => {
         const elementOffset = offset(section);
-
         inviewport = () => elementOffset < 150 && elementOffset >= -150;
-
         removeActive(section);
         addActive(inviewport(),section);
     });
@@ -52,18 +56,3 @@ const sectionActivation = () => {
 
 window.addEventListener('scroll' ,sectionActivation);
 
-
-const scrolling = () => {
-
-    const links = document.querySelectorAll('.navbar__menu a');
-    links.forEach(link => {
-        link.addEventListener('click', () => {
-            for(i = 0 ; i<sections ; i++){
-                sections[i].addEventListener("click",sectionScroll(link));
-            }
-        });
-    });
-
-};
-
-scrolling();
