88a7db3de84d2862767091440cac06574083a325
Tolga <68684561+TolgaS92@users.noreply.github.com>
2020-11-22 00:20:13 -0700

    Add files via upload

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -2,25 +2,30 @@ const navigation = document.getElementById('navbar__list');
 
 const sections = document.querySelectorAll('section');
 
-
-function scrollToSection(sectionID) {
-	window.scrollTo(0, sectionID);
-}
+const scroller = ( sectionID ) => {
+    window.scrollTo( 0, sectionID );
+};
 
 const navBuilder = () => {
 
     let navUI = '';
-    sections.forEach(section => {
 
+    sections.forEach( section => {
         const sectionID = section.id;
-      const sectionDataNav = section.dataset.nav;
-      let toOffSection = section.offsetTop + 30;
-
-        navUI += `<li><a class="menu__link"  name="#${sectionID}" onClick="scrollToSection(${toOffSection})">${sectionDataNav}</a></li>`;
-
-    });
+        const sectionDataNav = section.dataset.nav;
+        let toOffSection = section.offsetTop + 30;
+        
+        navUI += `<li><a class="menu__link"  id="#${sectionID}"  name="#${sectionID}" onClick="scroller(${toOffSection})">${sectionDataNav}</a></li>`;
+    } );
+    
     navigation.innerHTML = navUI;
 
+    
+    if ( sections ) {
+
+        document.getElementById( '#' + sections[ 0 ].id ).style.cssText = "background-color: grey;";
+
+        }
 
 };
 
@@ -30,29 +35,36 @@ const offset = (section) => {
     return Math.floor(section.getBoundingClientRect().top);
 };
 
-const removeActive = (section) => {
-    section.classList.remove('active');
+const dimmer = ( section ) => {
+    //console.log("removing highlight from navs #" + section.id);
+    let nav = document.getElementById('#' + section.id);
+    nav.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
     section.style.cssText = "background-color: linear-gradient(0deg, rgba(255,255,255,.1) 0%, rgba(255,255,255,.2) 100%)";
 };
 
-const addActive = (conditional, section) => {
+const highlight = ( conditional, section ) => {
+    let nav = document.getElementById('#' + section.id);
+    
     if(conditional){
-        section.classList.add('active');
       section.style.cssText = "background-color: grey;";
 
-      console.log("test active viewport " + section.id)
+        //console.log( "test active viewport " + section.id )
+        //console.log( "TEST HIGHLIGHTING NAV " + nav.id )
+        nav.style.cssText = "background-color: grey;";
     };
 };
 
 
-const sectionActivation = () => {
+const scrollHandler = () => {
     sections.forEach(section => {
         const elementOffset = offset(section);
-        inviewport = () => elementOffset < 150 && elementOffset >= -150;
-        removeActive(section);
-        addActive(inviewport(),section);
+        inviewport = () => elementOffset < 140 && elementOffset >= -140;
+        dimmer(section);
+        highlight( inviewport(), section );
+        
     });
 };
 
-window.addEventListener('scroll' ,sectionActivation);
+
+window.addEventListener("scroll", scrollHandler);
 
